5761fdbc78faabaedfa1a91108b820eec8749297
Aadarsh Shrestha <aadarsh.stha36@gmail.com>
2024-07-31 17:56:00 +0545

    reviewed

--- assets/Checkbox--qBzAaFg.js
+++
@@ -1,6 +0,0 @@
-import{v as i,j as e,H as l,K as x,aN as d}from"./index-CpGNqzAf.js";/**
- * @license lucide-react v0.395.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const m=i("MapPin",[["path",{d:"M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z",key:"2oe9fu"}],["circle",{cx:"12",cy:"10",r:"3",key:"ilqhr7"}]]),p=({label:c,name:o,control:s,isReadOnly:a,isRequired:n})=>e.jsx(l,{mb:4,isRequired:n,isReadOnly:a,children:e.jsx(x,{name:o,control:s,render:({field:{value:r,onChange:t}})=>e.jsx(d,{defaultChecked:r,colorScheme:"primary",value:r,onChange:t,children:c})})});export{p as C,m as M};
