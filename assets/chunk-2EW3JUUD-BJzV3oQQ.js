9e5c8077908c263141d8342c33f34f8ce4904424
Aadarsh Shrestha <aadarsh.stha36@gmail.com>
2024-08-06 16:55:54 +0545

    index

--- assets/chunk-2EW3JUUD-BJzV3oQQ.js
+++
@@ -1,1 +0,0 @@
-import{u as c}from"./chunk-YQO7BFFX-AjF0CZ_j.js";import{f as d,j as o,i as m,A as f}from"./index-CC8fYiGp.js";var x=d(function(a,r){const{className:s,...e}=a,t=c();return o.jsx(m.div,{ref:r,className:f("chakra-card__header",s),__css:t.header,...e})});export{x as C};
