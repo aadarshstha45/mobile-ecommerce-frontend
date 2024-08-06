672a0170d085d5a7a0652aab50d536dcbabb1b08
Aadarsh Shrestha <aadarsh.stha36@gmail.com>
2024-08-06 16:30:20 +0545

    buy-now

--- assets/Checkbox-DzrNf94Q.js
+++
@@ -1,6 +0,0 @@
-import{Q as i,j as e,a0 as l,a1 as x,aB as d}from"./index-BwlWQgOS.js";/**
- * @license lucide-react v0.395.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const m=i("MapPin",[["path",{d:"M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z",key:"2oe9fu"}],["circle",{cx:"12",cy:"10",r:"3",key:"ilqhr7"}]]),p=({label:c,name:o,control:a,isReadOnly:s,isRequired:n})=>e.jsx(l,{mb:4,isRequired:n,isReadOnly:s,children:e.jsx(x,{name:o,control:a,render:({field:{value:r,onChange:t}})=>e.jsx(d,{defaultChecked:r,colorScheme:"primary",value:r,onChange:t,children:c})})});export{p as C,m as M};
---
+++ assets/chunk-B5H2YLEF-CxDzGD0x.js
@@ -0,0 +1,1 @@
+import{u as r}from"./chunk-MGVPL3OH-B39vPdPa.js";import{f as a,j as e,i as f}from"./index-CC8fYiGp.js";var p=a((s,o)=>{const t=r();return e.jsx(f.tfoot,{...s,ref:o,__css:t.tfoot})});export{p as T};
