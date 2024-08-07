75a2121501479b8955072a2480bf92178c7ef8da
Aadarsh Shrestha <aadarsh.stha36@gmail.com>
2024-08-07 11:52:46 +0545

    esewa

--- assets/index-BKR5mTnT.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./index-BV0ivKrR.js";const s={getHomeSales:"get-sales-offer-products",get:"get-all-sales-offer"},a=()=>e({apiEndPoint:s.getHomeSales}),o=()=>e({apiEndPoint:s.get});export{a,o as u};
--- assets/index-D42K1mB3.js
+++
@@ -1,4 +0,0 @@
-import{al as a,am as d,bY as n}from"./index-BV0ivKrR.js";const r={post:"order",get:(t,o,e,s)=>`get-orders?page=${t}&&status=${o}
-  ${e?`&&date_from=${e}`:""}
-  ${s?`&&date_to=${s}`:""}
-  `,promo_code:"is-promocode-valid"},p=()=>a({apiEndPoint:r.post,inValidateEndpoint:n.get,message:"Order placed successfully"}),u=()=>a({apiEndPoint:r.promo_code}),c=(t,o,e,s)=>d(r.get(t,o,e,s));export{u as a,c as b,p as u};
