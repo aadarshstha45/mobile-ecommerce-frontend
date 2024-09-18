da5350dd20feb0aa8508c37dfbecd819ba84870f
Aadarsh Shrestha <aadarsh.stha36@gmail.com>
2024-09-18 17:30:34 +0545

    googleL

--- assets/chunk-7OLJDQMT-6j4Gq0v4.js
+++
@@ -1,1 +0,0 @@
-import{f as i,aI as t,o as r,j as c,h as o,E as m}from"./index-DBpBJgBg.js";var d=i(function(a,s){const e=t("Heading",a),{className:f,...n}=r(a);return c.jsx(o.h2,{ref:s,className:m("chakra-heading",a.className),...n,__css:e})});d.displayName="Heading";export{d as H};
