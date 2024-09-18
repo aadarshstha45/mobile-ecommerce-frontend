809f98c94d0d5ce17607e00f7fed2a26458fc3bf
Aadarsh Shrestha <aadarsh.stha36@gmail.com>
2024-09-18 13:00:43 +0545

    search

---
+++ assets/chunk-B5H2YLEF-Ca6TsuTe.js
@@ -0,0 +1,1 @@
+import{u as r}from"./chunk-MGVPL3OH-BRBwJfIi.js";import{f as a,j as e,h as f}from"./index-CHqn2l2u.js";var u=a((s,o)=>{const t=r();return e.jsx(f.tfoot,{...s,ref:o,__css:t.tfoot})});export{u as T};
