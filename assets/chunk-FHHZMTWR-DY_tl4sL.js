a430b537605b599e9ebb7046f34723d831f4c28f
Aadarsh Shrestha <aadarsh.stha36@gmail.com>
2024-09-18 17:42:56 +0545

    googleL

---
+++ assets/chunk-FHHZMTWR-DY_tl4sL.js
@@ -0,0 +1,1 @@
+import{u as c}from"./chunk-YQO7BFFX-B6cuy5OY.js";import{f as d,j as e,h as m,E as f}from"./index-BIeV2FSA.js";var u=d(function(s,a){const{className:r,...o}=s,t=c();return e.jsx(m.div,{ref:a,className:f("chakra-card__body",r),__css:t.body,...o})});export{u as C};
--- assets/index-CVifINUZ.js
+++
@@ -1,4 +0,0 @@
-import{ab as a,ac as d,c1 as n}from"./index-rCYdBeUT.js";const r={post:"order",get:(t,o,e,s)=>`get-orders?page=${t}&&status=${o}
-  ${e?`&&date_from=${e}`:""}
-  ${s?`&&date_to=${s}`:""}
-  `,promo_code:"is-promocode-valid"},i=()=>a({apiEndPoint:r.post,inValidateEndpoint:n.get,message:"Order placed successfully"}),p=()=>a({apiEndPoint:r.promo_code}),u=(t,o,e,s)=>d(r.get(t,o,e,s));export{p as a,u as b,i as u};
